a82f1b382c2dc0a575099776f7c417f58048b113
Jeremy Shih <jeremyliweishih@gmail.com>
2016-10-18 22:20:10 -0400

    setup pan to location

--- mbta/map.js
+++ mbta/map.js
@@ -13,5 +13,27 @@ var options = {
 function init()
 {
 	map = new google.maps.Map(document.getElementById("map_canvas"), options);
+
+	get_location();
 }
 
+function get_location()
+{
+	if (navigator.geolocation) {
+	  navigator.geolocation.getCurrentPosition(function(position) {
+	    lat = position.coords.latitude;
+	    long = position.coords.longitude;
+	    renderMap();
+	  });
+	}
+
+	else {
+	  alert("Sorry! Geolocation is not supported by your web browser.");
+	}
+}
+
+function renderMap()
+{
+	whereIAm = new google.maps.LatLng(lat, long);
+	map.panTo(whereIAm);
+}
